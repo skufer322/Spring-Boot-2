292f2eabf4d1f11957285a0d4361e274aa7eca8f
Stefan Kufer <stefan.kufer@diva-e.com>
2019-11-18 17:23:17 +0100

    korrekte Benennung der Dateien für Ausführung + Anpassungen POM/Properties

---
+++ src/main/resources/db/migration/V0001__create_database_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bundeslaender
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    kuerzel  VARCHAR(2)  NOT NULL,
+    landname VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ src/main/resources/db/migration/V0002__insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO bundeslaender (kuerzel, landname) VALUES ('BY', 'Bayern');
+INSERT INTO bundeslaender (kuerzel, landname) VALUES ('BW', 'Baden-Wuerttemberg');
+COMMIT;
