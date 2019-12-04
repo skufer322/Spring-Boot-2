eb63dc1ab67608519d3c78d5524fb4a0ef1b75c0
Stefan Kufer <stefan.kufer@diva-e.com>
2019-12-04 10:12:18 +0100

    kleine ergänzung

--- src/test/java/com/divae/sk/springboot2/people/PeopleControllerTest_MockBean.java
+++ src/test/java/com/divae/sk/springboot2/people/PeopleControllerTest_MockBean.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(PeopleController.class)
+@WebMvcTest(controllers = PeopleController.class)
 public class PeopleControllerTest_MockBean {
 
     @Autowired
